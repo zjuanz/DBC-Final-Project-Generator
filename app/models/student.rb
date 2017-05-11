e445870cb20701cc11253257ebdf7072e7aa9fbc
Ben Martin <email": null>
2017-05-11 10:00:24 -0500

    add student reg

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,5 +2,12 @@ class Student < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable, :registerable,
   # :recoverable, :rememberable,
-  devise :database_authenticatable
+  devise :database_authenticatable, :validatable
+
+  validates :first_name, :last_name, :email, presence: true
+  validates :email, uniqueness: true
+
+  def full_name
+    self.first_name + ' ' + self.last_name
+  end
 end
