5e43d116856dd448636d8bd00799f2a82e076886
Ben Martin <email": null>
2017-05-11 12:32:47 -0500

    Add show and index for User

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 Rails.application.routes.draw do
-  resources :cohorts 
-  devise_for :students
+  get 'students/index'
+
+  resources :cohorts
+  devise_for :students, :controllers => {:registration => 'registrations'}
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get '/students', to: 'students#index'
+  get '/students/:id', to: 'students#show'
 
   get '/' => "application#login"
 end
