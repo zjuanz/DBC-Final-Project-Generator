d1d3d1b8014da8027987d40b530001bc4d913c6a
Ben Martin <email": null>
2017-05-11 11:40:50 -0500

    Add Student seed data, fix views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,9 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cohort.destroy_all
+Student.destroy_all
+
 Cohort.create(name: "Bumblebee", active: "true", pitch_limit: 2, pitch_start: "2017-05-10", vote_start: "2017-05-11", vote_limit: 3, vote_end: "2017-05-12")
 
 Cohort.create(name: "Fireball", active: "false", pitch_limit: 2, pitch_start: "2017-05-10", vote_start: "2017-05-11", vote_limit: 3, vote_end: "2017-05-12")
 
 Cohort.create(name: "Fox", active: "true", pitch_limit: 2, pitch_start: "2017-05-10", vote_start: "2017-05-11", vote_limit: 3, vote_end: "2017-05-12")
 
+10.times do
+  student = {
+    :first_name => Faker::Name.first_name,
+    :last_name => Faker::Name.last_name,
+    :email => Faker::Internet.email,
+    :cohort => Cohort.first,
+    :password => 'password'
+  }
+  Student.create(student)
+end
