d16e3e55b388498e2567967debfca8c5ca80542b
Rafael Lara <ing.lara@live.com>
2017-05-12 10:47:57 -0500

    Add votes and add round id to votes

--- app/models/pitch.rb
+++ app/models/pitch.rb
@@ -3,8 +3,11 @@ class Pitch < ApplicationRecord
   validate :description
   validate :student_id 
 
+
   belongs_to :student
+  belongs_to :cohort, foreign_key: :student_id
   has_many :votes
+
   
 
 end
