9364a7b1a5a8207ab9472ea91b7c3307fe47cee1
Ben Martin <benjamin.martin2010@gmail.com>
2017-05-13 16:07:15 -0500

    Started working on create team page

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -1,8 +1,16 @@
 class TeamsController < ApplicationController
   def new
-    @cohort = Cohort.find(flash[:id])
+    @cohort = Cohort.find(1)
     @pitches = @cohort.current_pitches
-    @votes = @pitches.each {|pitch| pitch.votes}
+    @votes = {}
+    a = []
+    @pitches.each do |pitch|
+      a << pitch.votes.select do |vote|
+        vote.round_id == @cohort.stage_id
+      end
+      @votes[pitch] = a
+    end
+    binding.pry
   end
 
 end
