38486452de58dbad4d1a04c647e0774b96f9027a
Juan Zalles <juan.zalles@gmail.com>
2017-05-13 06:19:13 -0500

    add default user to seed, fix layout problems

--- db/seeds.rb
+++ db/seeds.rb
@@ -31,7 +31,13 @@ Cohort.create(name: "Bumblebee", active: "true", pitch_limit: 2, pitch_start: "2
 Cohort.create(name: "Fireball", active: "false", pitch_limit: 2, pitch_start: "2017-05-10", vote_start: "2017-05-11", vote_limit: 3, vote_end: "2017-05-12", stage_id:0)
 
 Cohort.create(name: "Fox", active: "true", pitch_limit: 2, pitch_start: "2017-05-10", vote_start: "2017-05-11", vote_limit: 3, vote_end: "2017-05-12", stage_id:0)
-
+Student.create(
+  :first_name => "Uber",
+  :last_name => "Student",
+  :email => "student@hello.com",
+  :cohort => Cohort.last,
+  :password => "password"
+  )
 10.times do
   student = {
     :first_name => Faker::Name.first_name,
