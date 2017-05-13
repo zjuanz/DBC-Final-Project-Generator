e4de8244a1aa026966d110dc2212d59e2e493fd7
Benjamin Martin <benjamin.martin2010@gmail.com>
2017-05-13 13:52:25 +0000

    rounds

--- app/controllers/rounds_controller.rb
+++ app/controllers/rounds_controller.rb
@@ -6,9 +6,22 @@ class RoundsController < ApplicationController
   def create
     @cohort = Cohort.find(params[:cohort_id])
     if params[:round_status] == 1
-      # NEW ROUND
+      new_round(@cohort, params[:pitch_ids])
+      redirect_to 
     else
       # NEW TEAMS
     end
+  end
+
+
+  private
+
+  def new_round(cohort, pitch_ids)
+    pitch_ids.each do |pitch_id|
+      pitch = Pitch.find(pitch_id)
+      pitch.round_id += 1
+      pitch.save
+    end
+    @cohort.stage_id += 1
   end
 end
